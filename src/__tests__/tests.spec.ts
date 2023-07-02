16bea90fe368e65f8b692a25ef0fc174b0970dc1
Vladimir Orlov <satancrew.true@gmail.com>
2023-07-02 17:40:31 +0300

    feat: add few scenarious for tests

--- src/__tests__/tests.spec.ts
+++ src/__tests__/tests.spec.ts
@@ -3,6 +3,8 @@ import request from 'supertest';
 import { User, UserWithoutId } from '../helpers/types';
 import { validate, v4 as uuidv4 } from 'uuid';
 
+let newUser;
+
 describe('Get all users', () => {
   it('should return empty array', async () => {
     const response = await request(server).get('/api/users');
@@ -18,19 +20,41 @@ describe('Create new user with POST', () => {
     hobbies: ['node', 'typescript', 'stradanie'],
   };
 
-  let newUser;
+  const wrongUser = {
+    username: 52,
+    age: 'eleven',
+    job: false,
+  }
 
-  it('create new user and testing gis data', async () => {
+  it('create new user and testing his data', async () => {
     const response = await request(server).post('/api/users').send(testUser);
     expect(response.statusCode).toBe(201);
     expect(validate(response.body.id)).toBe(true);
     expect(response.body.age).toBe(testUser.age);
     expect(response.body.username).toBe(testUser.username);
     expect(response.body.hobbies).toEqual(testUser.hobbies);
-    expect(Object.keys(response.body).length).toBe(4);
-
     newUser = response.body;
   });
+  it('got error when create user with wrong type', async () => {
+    const response = await request(server).post('/api/users').send(wrongUser);
+    expect(response.statusCode).toBe(400);
+  })
+});
+
+describe('Get created user with GET method', () => {
+  it('get created user by his id', async () => {
+    const testingId = newUser.id;
+    const response = await request(server).get('/api/users/' + testingId);
+    expect(response.statusCode).toBe(200);
+    expect(response.body.age).toBe(newUser.age);
+    expect(response.body.username).toBe(newUser.username);
+    expect(response.body.hobbies).toEqual(newUser.hobbies);
+  });
+  it('get error when we trying get user with wrong id', async () => {
+    const response = await request(server).get('/api/users/randomId');
+    expect(response.statusCode).toBe(404);
+  })
+
 });
 
 afterAll(async () => {
